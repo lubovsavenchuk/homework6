93ed22ca22f88934f80c13ddbbc5255cbfecc4f0
Савенчук Любовь <sav_lu@gmail.com>
2022-09-10 20:16:26 +0500

    ex1/

---
+++ ex1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Программа запрашивает натуральное число M, пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Input quantity of numbers: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int [] arr = new int[M];
+int count = 0;
+for(int i = 0; i < arr.Length; i++)
+{
+   Console.Write($"Input {i} number:\t");
+   arr[i] = Convert.ToInt32(Console.ReadLine());
+   
+}
+Console.WriteLine("Here are the numbers: ");
+for(int i = 0; i < arr.Length; i++)
+{
+   Console.WriteLine(arr[i]);
+   if(arr[i] > 0)
+   {
+      count++;
+   }
+}
+Console.WriteLine($"Quantity of positive numbers is: {count}");
+
+
